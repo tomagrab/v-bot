c3f05d15462e066f10b44d6295fdd06592eb3644
Tommy Boi <tomaarongrabowski@gmail.com>
2023-08-24 19:37:44 -0400

    Updated UI

--- src/lib/Navbar/Navbar.svelte
+++ src/lib/Navbar/Navbar.svelte
@@ -2,14 +2,30 @@
   class="navbar bg-base-100 sticky top-0 left-0 right-0 w-full backdrop-blur"
 >
   <div class="navbar-start">
-    <img src="/logo.png" alt="V-Track Logo" width="200" />
+    <label for="my-drawer" class="btn btn-ghost drawer-button"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h7"
+        /></svg
+      ></label
+    >
   </div>
   <div class="navbar-cemter">
     <a class="btn btn-ghost normal-case text-xl" href="/"
       >V-Bot - An AI Chatbot</a
     >
   </div>
-  <div class="navbar-end" />
+  <div class="navbar-end">
+    <img src="/logo.png" alt="V-Track Logo" width="200" />
+  </div>
 </div>
 
 <style lang="scss">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import AnimatedRoute from "$lib/AnimatedRoute/AnimatedRoute.svelte";
+  import Footer from "$lib/Footer/Footer.svelte";
   import Navbar from "$lib/Navbar/Navbar.svelte";
   import "../app.scss";
 </script>
@@ -8,31 +9,32 @@
   <title>V-Bot</title>
 </svelte:head>
 
-<div class="app min-h-screen flex flex-col">
-  <Navbar />
-
-  <div class="flex-grow flex">
-    <aside class="sidebar basis-1/3 flex flex-col items-center px-4">
-      <ul>
-        <li><a href="/UserGuide">User Guide</a></li>
-        <li><a href="/Features">Features</a></li>
-        <li><a href="/Information">Information</a></li>
-        <li><a href="/FAQs">FAQs</a></li>
-      </ul>
-    </aside>
-    <main class="main basis-2/3 px-4">
-      <AnimatedRoute>
-        <div class="flex flex-col items-center pb-4">
-          <slot />
-        </div>
-      </AnimatedRoute>
-    </main>
+<div class="drawer">
+  <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    <div class="app min-h-screen flex flex-col">
+      <Navbar />
+      <div class="flex-grow">
+        <AnimatedRoute>
+          <div class="flex flex-col items-center pb-4">
+            <slot />
+          </div>
+        </AnimatedRoute>
+      </div>
+      <Footer />
+    </div>
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer" class="drawer-overlay" />
+    <ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+      <!-- Sidebar content here -->
+      <li><a href="/UserGuide">User Guide</a></li>
+      <li><a href="/Features">Features</a></li>
+      <li><a href="/Information">Information</a></li>
+      <li><a href="/FAQs">FAQs</a></li>
+    </ul>
   </div>
 </div>
 
 <style lang="scss">
-  @import "../app.scss";
-  .sidebar__title {
-    border-bottom: 1px solid $light;
-  }
 </style>
